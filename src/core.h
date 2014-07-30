d52812c11323b281f487ecd4af9870c8fa15f0d8
sidhujag <sidhujag@hotmail.com>
2014-07-29 17:16:28 -0700

    x

--- src/core.h
+++ src/core.h
@@ -16,8 +16,16 @@
 #include <vector>
 #include <boost/shared_ptr.hpp>
 #include "auxpow.h"
+class CAuxPow;
 class CTransaction;
+template <typename Stream>
+int ReadWriteAuxPow(Stream& s, const boost::shared_ptr<CAuxPow>& auxpow, int nType, int nVersion, CSerActionGetSerializeSize ser_action);
 
+template <typename Stream>
+int ReadWriteAuxPow(Stream& s, const boost::shared_ptr<CAuxPow>& auxpow, int nType, int nVersion, CSerActionSerialize ser_action);
+
+template <typename Stream>
+int ReadWriteAuxPow(Stream& s, boost::shared_ptr<CAuxPow>& auxpow, int nType, int nVersion, CSerActionUnserialize ser_action);
 enum BlockStatus {
     BLOCK_VALID_UNKNOWN      =    0,
     BLOCK_VALID_HEADER       =    1, // parsed, version ok, hash satisfies claimed PoW, 1 <= vtx count <= max, timestamp not in future
