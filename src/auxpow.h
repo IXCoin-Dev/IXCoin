2adae0834cabfb6d445d97819d7b626703bed4a3
sidhujag <sidhujag@hotmail.com>
2014-07-29 18:19:37 -0700

    d

--- src/auxpow.h
+++
@@ -1,9 +0,0 @@
-// Copyright (c) 2009-2010 Satoshi Nakamoto
-// Distributed under the MIT/X11 software license, see the accompanying
-// file license.txt or http://www.opensource.org/licenses/mit-license.php.
-#ifndef BITCOIN_AUXPOW_H
-#define BITCOIN_AUXPOW_H
-
-#include "main.h"
-
-#endif
