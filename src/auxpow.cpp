25d73dcd4d1ab16fae753d30ae834c2a9ac8056d
sidhujag <sidhujag@hotmail.com>
2014-07-29 17:29:33 -0700

    d

--- src/auxpow.cpp
+++ src/auxpow.cpp
@@ -3,8 +3,9 @@
 // file license.txt or http://www.opensource.org/licenses/mit-license.php.
 #include "script.h"
 #include "init.h"
-#include "auxpow.h"
 #include "main.h"
+#include "auxpow.h"
+
 
 using namespace std;
 using namespace boost;
